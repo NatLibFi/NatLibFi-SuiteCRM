43fd769e94f237eced52001f6593f265a5b0cd0d
Leszek Manicki <lebiega@gmail.com>
2017-11-17 12:15:48 +0200

    Restore accidentally removed custom Quick Create form for Alliances
    Used in the Roles in Alliances subpanel of the Contacts view.
    
    Part of ASKI-164

--- custom/modules/nlfal_Alliances/metadata/quickcreatedefs.php
+++ custom/modules/nlfal_Alliances/metadata/quickcreatedefs.php
@@ -1,4 +1,61 @@
 <?php
+
+$returnModule = 'default';
+if (isset($_REQUEST['return_module'])) {
+    $returnModule = $_REQUEST['return_module'];
+}
+
+$fields = array();
+
+if ($returnModule === 'Contacts') {
+    $fields = array( array(
+        array(
+            'name' => 'nlfal_alliances_contacts_1_alliance_enum',
+            'label' => 'LBL_NLFAL_ALLIANCES_CONTACTS_1_ALLIANCE_TITLE',
+            'displayParams' => array( 'idName' => 'nlfal_alliances_contacts_1nlfal_alliances_ida' ),
+        ),
+        /*), array(*/ array(
+            'name' => 'nlfal_alliances_contacts_1_role_enum',
+            'label' => 'LBL_NLFAL_ALLIANCES_CONTACTS_1_ROLE',
+            'displayParams' =>
+            array (
+                'idName' => 'role',
+            ),
+        ),
+    ) );
+}
+
+if (empty($fields)) {
+    $fields = array (
+        0 => 
+        array (
+          0 => 'name',
+          1 => 
+          array (
+            'name' => 'status_c',
+            'label' => 'LBL_STATUS',
+          ),
+        ),
+        1 => 
+        array (
+          1 => 
+          array (
+            'name' => 'sektorit_c',
+            'label' => 'LBL_SEKTORIT',
+          ),
+        ),
+        2 => 
+        array (
+          0 => 
+          array (
+            'name' => 'description',
+            'comment' => 'Full text of the note',
+            'label' => 'LBL_DESCRIPTION',
+          ),
+        ),
+      );
+}
+
 $module_name = 'nlfal_Alliances';
 $viewdefs [$module_name] = 
 array (
@@ -29,46 +86,13 @@ array (
           'panelDefault' => 'expanded',
         ),
       ),
-      'includes' => 
-      array (
-        0 => 
-        array (
-        ),
+      'includes' => array(
+         $returnModule === 'Contacts' ? array( 'file' => 'custom/modules/nlfal_Alliances/roles_alliances.js' ) : array(),
       ),
     ),
     'panels' => 
     array (
-      'default' => 
-      array (
-        0 => 
-        array (
-          0 => 'name',
-          1 => 
-          array (
-            'name' => 'status_c',
-            'studio' => 'visible',
-            'label' => 'LBL_STATUS',
-          ),
-        ),
-        1 => 
-        array (
-          0 => 
-          array (
-            'name' => 'sektorit_c',
-            'studio' => 'visible',
-            'label' => 'LBL_SEKTORIT',
-          ),
-        ),
-        2 => 
-        array (
-          0 => 
-          array (
-            'name' => 'description',
-            'comment' => 'Full text of the note',
-            'label' => 'LBL_DESCRIPTION',
-          ),
-        ),
-      ),
+      'default' => $fields,
     ),
   ),
 );
