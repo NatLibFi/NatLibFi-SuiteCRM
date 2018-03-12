f4a0087bc2c502939b628ab2ab84363251f7b76b
Leszek Manicki <lebiega@gmail.com>
2018-03-12 22:24:20 +0100

    Fake Suite7 theme's compatibility with SuiteCRM 7

--- themes/Suite7/themedef.php
+++ themes/Suite7/themedef.php
@@ -43,7 +43,7 @@ $themedef = array(
     'name'  => "Suite 7",
     'description' => "SuiteCRM 7.0 Theme",
     'version' => array(
-        'regex_matches' => array('6\.*.*'),
+        'regex_matches' => array('.+'),
         ),
     'group_tabs' => true,
     'classic' => true,
