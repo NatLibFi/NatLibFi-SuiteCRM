08ad689c859c77eb4f63eb917775f541c08401a0
Leszek Manicki <lebiega@gmail.com>
2018-03-20 16:40:37 +0200

    Added initial shared config file
    Contains settings that can be stored in version control
    system, and are meant to be shared across all NLF's
    instances.
    
    Initial version makes it easier to resolve ASKI-212.

---
+++ aski_config.php
@@ -0,0 +1,25 @@
+<?php
+
+// Publicly viewable settings shared across National Library of Finland's
+// SuiteCRM instances (ASKI project).
+// This file is supposed to be loaded from config.php.
+// Config values can be overwritten on an instance-specific basis
+// through config_override.php.
+
+$sugar_config['enable_legacy_search'] = array(
+  'Accounts',
+  'nlfal_Alliances',
+  'nlfbr_BusinessRelationships',
+  'Contacts',
+  'Leads',
+  'nlfse_Services',
+  'AOS_Contracts',
+  'nlfwg_WorkingGroups',
+  'nlfbs_BackendSystems',
+  'ProspectLists',
+  'Emails',
+  'AOR_Reports',
+  'Project',
+  'Campaigns',
+  'AOS_Invoices',
+);
