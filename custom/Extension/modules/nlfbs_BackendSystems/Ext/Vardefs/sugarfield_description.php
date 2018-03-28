660b1afaaeb14538204bb657c91ca566f03f698b
Leszek Manicki <lebiega@gmail.com>
2018-03-28 18:30:44 +0300

    Register changes to Backend System fields in the audit/change log
    Part of ASKI-205

---
+++ custom/Extension/modules/nlfbs_BackendSystems/Ext/Vardefs/sugarfield_description.php
@@ -0,0 +1,8 @@
+<?php
+ // created: 2018-03-28 18:28:12
+$dictionary['nlfbs_BackendSystems']['fields']['description']['audited']=true;
+$dictionary['nlfbs_BackendSystems']['fields']['description']['inline_edit']=true;
+$dictionary['nlfbs_BackendSystems']['fields']['description']['comments']='Full text of the note';
+$dictionary['nlfbs_BackendSystems']['fields']['description']['merge_filter']='disabled';
+
+ ?>
