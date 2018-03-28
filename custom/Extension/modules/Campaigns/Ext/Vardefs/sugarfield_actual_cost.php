bbd2ec4c3e52ff0615a47ebda68ab8ffba2e4795
Leszek Manicki <lebiega@gmail.com>
2018-03-28 19:21:18 +0300

    Store changes to Campaign fields in the audit/change log
    Part of ASKI-205.

---
+++ custom/Extension/modules/Campaigns/Ext/Vardefs/sugarfield_actual_cost.php
@@ -0,0 +1,9 @@
+<?php
+ // created: 2018-03-28 19:18:25
+$dictionary['Campaign']['fields']['actual_cost']['audited']=true;
+$dictionary['Campaign']['fields']['actual_cost']['inline_edit']=true;
+$dictionary['Campaign']['fields']['actual_cost']['comments']='Actual cost of the campaign';
+$dictionary['Campaign']['fields']['actual_cost']['merge_filter']='disabled';
+$dictionary['Campaign']['fields']['actual_cost']['enable_range_search']=false;
+
+ ?>
