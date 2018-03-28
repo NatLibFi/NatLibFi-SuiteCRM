3cb4db93cd51765e332895d035a1061627a3d3d2
Leszek Manicki <lebiega@gmail.com>
2018-03-28 18:06:02 +0300

    Register changes to Alliance fields to audit/change log
    Part of ASKI-205.

--- custom/Extension/modules/nlfal_Alliances/Ext/Vardefs/sugarfield_description.php
+++ custom/Extension/modules/nlfal_Alliances/Ext/Vardefs/sugarfield_description.php
@@ -1,7 +1,8 @@
 <?php
- // created: 2017-01-04 11:27:33
+ // created: 2018-03-28 17:50:57
 $dictionary['nlfal_Alliances']['fields']['description']['inline_edit']=true;
 $dictionary['nlfal_Alliances']['fields']['description']['comments']='Full text of the note';
 $dictionary['nlfal_Alliances']['fields']['description']['merge_filter']='disabled';
+$dictionary['nlfal_Alliances']['fields']['description']['audited']=true;
 
  ?>
--- custom/Extension/modules/nlfal_Alliances/Ext/Vardefs/sugarfield_name.php
+++ custom/Extension/modules/nlfal_Alliances/Ext/Vardefs/sugarfield_name.php
@@ -1,10 +1,11 @@
 <?php
- // created: 2016-12-29 10:36:42
+ // created: 2018-03-28 17:51:04
 $dictionary['nlfal_Alliances']['fields']['name']['inline_edit']=true;
 $dictionary['nlfal_Alliances']['fields']['name']['comments']='Kimpan nimi';
 $dictionary['nlfal_Alliances']['fields']['name']['duplicate_merge']='disabled';
 $dictionary['nlfal_Alliances']['fields']['name']['duplicate_merge_dom_value']='0';
 $dictionary['nlfal_Alliances']['fields']['name']['merge_filter']='disabled';
 $dictionary['nlfal_Alliances']['fields']['name']['unified_search']=false;
+$dictionary['nlfal_Alliances']['fields']['name']['audited']=true;
 
  ?>
