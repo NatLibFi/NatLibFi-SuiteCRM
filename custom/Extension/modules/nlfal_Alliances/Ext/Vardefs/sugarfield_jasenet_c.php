8d7e8366b2346147239d63021327315e170c35d3
Leszek Manicki <lebiega@gmail.com>
2018-04-20 23:29:34 +0300

    Removed redundant Alliance's jasenet_c field

--- custom/Extension/modules/nlfal_Alliances/Ext/Vardefs/sugarfield_jasenet_c.php
+++
@@ -1,6 +0,0 @@
-<?php
- // created: 2017-03-17 13:54:49
-$dictionary['nlfal_Alliances']['fields']['jasenet_c']['inline_edit']='1';
-$dictionary['nlfal_Alliances']['fields']['jasenet_c']['labelValue']='Jäsenet';
-
- ?>
