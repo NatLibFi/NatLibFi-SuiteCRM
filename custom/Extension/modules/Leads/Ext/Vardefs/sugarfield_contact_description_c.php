ce168d3e37328cbff3809ad8b98d65b469ba3b1a
Leszek Manicki <lebiega@gmail.com>
2018-05-10 20:44:58 +0300

    Updated some field metadata (missed from previous commits)
    Part of ASKI-191

--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_contact_description_c.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_contact_description_c.php
@@ -1,6 +1,6 @@
 <?php
- // created: 2017-08-25 22:52:56
+ // created: 2018-05-07 22:40:53
 $dictionary['Lead']['fields']['contact_description_c']['inline_edit']='1';
-$dictionary['Lead']['fields']['contact_description_c']['labelValue']='Lisätiedot';
+$dictionary['Lead']['fields']['contact_description_c']['labelValue']='Yhteyshenkilön lisätiedot';
 
  ?>
