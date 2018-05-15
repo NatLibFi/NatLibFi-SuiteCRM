77e4a077a8bd202a160ea65a86358a72a17dc02d
Leszek Manicki <lebiega@gmail.com>
2018-05-16 00:35:51 +0300

    Remove top buttons of the BR's Leads subpanel
    Part of ASKI-191

--- custom/Extension/modules/nlfbr_BusinessRelationships/Ext/Layoutdefs/nlfbr_businessrelationships_leads_1_nlfbr_BusinessRelationships.php
+++ custom/Extension/modules/nlfbr_BusinessRelationships/Ext/Layoutdefs/nlfbr_businessrelationships_leads_1_nlfbr_BusinessRelationships.php
@@ -8,16 +8,5 @@ $layout_defs["nlfbr_BusinessRelationships"]["subpanel_setup"]['nlfbr_businessrel
   'sort_by' => 'id',
   'title_key' => 'LBL_NLFBR_BUSINESSRELATIONSHIPS_LEADS_1_FROM_LEADS_TITLE',
   'get_subpanel_data' => 'nlfbr_businessrelationships_leads_1',
-  'top_buttons' => 
-  array (
-    0 => 
-    array (
-      'widget_class' => 'SubPanelTopButtonQuickCreate',
-    ),
-    1 => 
-    array (
-      'widget_class' => 'SubPanelTopSelectButton',
-      'mode' => 'MultiSelect',
-    ),
-  ),
+  'top_buttons' => '',
 );
