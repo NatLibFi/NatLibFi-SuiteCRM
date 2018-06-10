a01e101e6ae390d8e2ea0cc0da3347b837238129
Leszek Manicki <lebiega@gmail.com>
2018-06-10 19:09:52 +0300

    Updated Accounts subpanel of Alliances
    Changes made by Susannsa Eklund.

--- custom/modules/Accounts/metadata/subpanels/nlfal_Alliances_subpanel_nlfal_alliances_accounts_1.php
+++ custom/modules/Accounts/metadata/subpanels/nlfal_Alliances_subpanel_nlfal_alliances_accounts_1.php
@@ -1,21 +1,55 @@
 <?php
-// created: 2017-05-03 16:34:37
+// created: 2018-04-23 16:02:04
 $subpanel_layout['list_fields'] = array (
   'name' => 
   array (
-  //  'type' => 'name',
     'vname' => 'LBL_NAME',
     'width' => '10%',
     'default' => true,
     'widget_class' => 'SubPanelDetailViewLink',
   ),
-  'account_backend_system_names' =>
-  array(
+  'parent_name' => 
+  array (
+    'type' => 'relate',
+    'link' => true,
+    'vname' => 'LBL_MEMBER_OF',
+    'id' => 'PARENT_ID',
+    'width' => '10%',
+    'default' => true,
+    'widget_class' => 'SubPanelDetailViewLink',
+    'target_module' => 'Accounts',
+    'target_record_key' => 'parent_id',
+  ),
+  'account_backend_system_names' => 
+  array (
     'type' => 'varchar',
     'vname' => 'LBL_ACCOUNT_BACKEND_SYSTEMS_TITLE',
     'width' => '10%',
     'default' => true,
     'sortable' => false,
+  ),
+  'industry' => 
+  array (
+    'type' => 'enum',
+    'vname' => 'LBL_INDUSTRY',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'hallinnonala_c' => 
+  array (
+    'type' => 'enum',
+    'default' => true,
+    'studio' => 'visible',
+    'vname' => 'LBL_HALLINNONALA',
+    'width' => '10%',
+  ),
+  'organisaatio_status_c' => 
+  array (
+    'type' => 'enum',
+    'default' => true,
+    'studio' => 'visible',
+    'vname' => 'LBL_ORGANISAATIO_STATUS',
+    'width' => '10%',
   ),
   'edit_button' => 
   array (
