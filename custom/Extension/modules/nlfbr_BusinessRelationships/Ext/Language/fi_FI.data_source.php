c3877cb004658d414973f2e4871ccd156efa30cb
Leszek Manicki <lebiega@gmail.com>
2018-06-12 21:28:30 +0300

    Fixed the spelling of the label of Melinda Database ID field
    As noticed in ASKI-256

--- custom/Extension/modules/nlfbr_BusinessRelationships/Ext/Language/fi_FI.data_source.php
+++ custom/Extension/modules/nlfbr_BusinessRelationships/Ext/Language/fi_FI.data_source.php
@@ -2,7 +2,7 @@
 
 $mod_strings['LBL_DATA_SOURCE_TITLE'] = 'Tietolähde';
 $mod_strings['LBL_DATA_SOURCE_NAME_TITLE'] = 'Tietolähteen nimi:';
-$mod_strings['LBL_DATA_SOURCE_DATABASE_ID_TITLE'] = 'Tietokantatunnus:';
+$mod_strings['LBL_DATA_SOURCE_DATABASE_ID_TITLE'] = 'Tietokannan tunnus:';
 $mod_strings['LBL_DATA_SOURCE_BACKEND_SYSTEM_TITLE'] = 'Palveluun liitettävä tietojärjestelmä:';
 $mod_strings['LBL_DATA_SOURCE_HARVESTING_FORMAT_TITLE'] = 'Haravointiformaatti:';
 $mod_strings['LBL_DATA_SOURCE_CONTACT_EMAIL_TITLE'] = 'Aineistoa koskevan palautteen e-mail:';
