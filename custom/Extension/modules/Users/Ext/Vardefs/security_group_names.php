d6ffdf9537ac9911f40d53ebd60466ad0a895a4b
Leszek Manicki <lebiega@gmail.com>
2018-06-17 15:51:49 +0300

    Added security group name column to Users list view
    Resolves ASKI-268

---
+++ custom/Extension/modules/Users/Ext/Vardefs/security_group_names.php
@@ -0,0 +1,10 @@
+<?php
+
+$dictionary["User"]["fields"]["security_group_names"] = array (
+    'name' => 'security_group_names',
+    'type' => 'varchar',
+    'source' => 'non-db',
+    'vname' => 'LBL_SECURITY_GROUP_NAMES',
+    'studio' => array('detailview' => false, 'editview' => false, 'listview' => true, 'searchview' => false,),
+);
+
