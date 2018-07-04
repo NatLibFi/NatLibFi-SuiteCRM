dce693a7792db1d146b5692798d2ec3f1d55dcb2
Leszek Manicki <lebiega@gmail.com>
2018-07-04 22:14:29 +0300

    Also changed label of Finna view status field in Detail/Edit view
    Part of ASKI-272

--- custom/Extension/modules/nlfbr_BusinessRelationships/Ext/Language/fi_FI.finna_view.php
+++ custom/Extension/modules/nlfbr_BusinessRelationships/Ext/Language/fi_FI.finna_view.php
@@ -1,7 +1,7 @@
 <?php
 
 $mod_strings['LBL_FINNA_VIEW_TITLE'] = 'Oma näkymä';
-$mod_strings['LBL_FINNA_VIEW_STATUS_TITLE'] = 'Oma Finna-näkymä:';
+$mod_strings['LBL_FINNA_VIEW_STATUS_TITLE'] = 'Finna-näkymän tilanne:';
 $mod_strings['LBL_FINNA_VIEW_URL_TITLE'] = 'Oman Finna -näkymän osoite:';
 $mod_strings['LBL_FINNA_VIEW_ADMIN_ACCESS_TITLE'] = 'Paasy hallintaliittymään?';
 $mod_strings['LBL_FINNA_VIEW_PRODUCTION_DATE_TITLE'] = 'Näkymän tuotantoonsiirtymispäivä:';
