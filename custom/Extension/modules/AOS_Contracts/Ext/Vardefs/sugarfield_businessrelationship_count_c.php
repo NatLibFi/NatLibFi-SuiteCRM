2184ff21308b295abcec571bcc626dabe665072e
Leszek Manicki <lebiega@gmail.com>
2018-07-17 23:05:33 +0300

    Made dynamic field of Contracts non sortable in list views
    Part of ASKI-284

--- custom/Extension/modules/AOS_Contracts/Ext/Vardefs/sugarfield_businessrelationship_count_c.php
+++ custom/Extension/modules/AOS_Contracts/Ext/Vardefs/sugarfield_businessrelationship_count_c.php
@@ -2,5 +2,6 @@
  // created: 2017-03-09 01:17:49
 $dictionary['AOS_Contracts']['fields']['businessrelationship_count_c']['inline_edit']='';
 $dictionary['AOS_Contracts']['fields']['businessrelationship_count_c']['labelValue']='Allekirjoitettujen sopimusten määrä';
+$dictionary['AOS_Contracts']['fields']['businessrelationship_count_c']['sortable'] = false;
 
  ?>
