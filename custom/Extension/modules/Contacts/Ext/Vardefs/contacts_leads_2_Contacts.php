433f93a5efe3c0e5d406638753ef6c89860a8368
Leszek Manicki <lebiega@gmail.com>
2018-07-21 20:37:51 +0300

    Added many-to-many relationships between Contacts and Leads
    Part of ASKI-274

---
+++ custom/Extension/modules/Contacts/Ext/Vardefs/contacts_leads_2_Contacts.php
@@ -0,0 +1,11 @@
+<?php
+// created: 2018-07-21 19:35:41
+$dictionary["Contact"]["fields"]["contacts_leads_2"] = array (
+  'name' => 'contacts_leads_2',
+  'type' => 'link',
+  'relationship' => 'contacts_leads_2',
+  'source' => 'non-db',
+  'module' => 'Leads',
+  'bean_name' => 'Lead',
+  'vname' => 'LBL_CONTACTS_LEADS_2_FROM_LEADS_TITLE',
+);
---
+++ custom/Extension/modules/relationships/layoutdefs/contacts_leads_2_Leads.php
@@ -0,0 +1,23 @@
+<?php
+ // created: 2018-07-21 19:35:41
+$layout_defs["Leads"]["subpanel_setup"]['contacts_leads_2'] = array (
+  'order' => 100,
+  'module' => 'Contacts',
+  'subpanel_name' => 'default',
+  'sort_order' => 'asc',
+  'sort_by' => 'id',
+  'title_key' => 'LBL_CONTACTS_LEADS_2_FROM_CONTACTS_TITLE',
+  'get_subpanel_data' => 'contacts_leads_2',
+  'top_buttons' => 
+  array (
+    0 => 
+    array (
+      'widget_class' => 'SubPanelTopButtonQuickCreate',
+    ),
+    1 => 
+    array (
+      'widget_class' => 'SubPanelTopSelectButton',
+      'mode' => 'MultiSelect',
+    ),
+  ),
+);
