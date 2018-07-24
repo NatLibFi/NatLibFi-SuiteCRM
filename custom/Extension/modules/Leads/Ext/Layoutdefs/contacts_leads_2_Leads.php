565c05f431f8a23831ac70e134af0c562a1a5c02
Leszek Manicki <lebiega@gmail.com>
2018-07-24 23:29:59 +0300

    Temporarily hide the Contacts subpanel on Leads
    Until ASKI-274 is fully specified

--- custom/Extension/modules/Leads/Ext/Layoutdefs/contacts_leads_2_Leads.php
+++ custom/Extension/modules/Leads/Ext/Layoutdefs/contacts_leads_2_Leads.php
@@ -1,6 +1,6 @@
 <?php
  // created: 2018-07-21 19:35:41
-$layout_defs["Leads"]["subpanel_setup"]['contacts_leads_2'] = array (
+$layout_defs["Leads"]["subpanel_setup"]['contacts_leads_2'] = '';/*array (
   'order' => 100,
   'module' => 'Contacts',
   'subpanel_name' => 'ForLeads',
@@ -8,4 +8,4 @@ $layout_defs["Leads"]["subpanel_setup"]['contacts_leads_2'] = array (
   'sort_by' => 'id',
   'title_key' => 'LBL_CONTACTS_LEADS_2_FROM_CONTACTS_TITLE',
   'get_subpanel_data' => 'contacts_leads_2',
-);
+);*/
