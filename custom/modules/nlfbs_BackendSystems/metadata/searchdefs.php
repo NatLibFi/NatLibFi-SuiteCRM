6719807e8e2d8568f18a9b4fcc7184bb2bae30d4
Leszek Manicki <lebiega@gmail.com>
2018-07-25 00:10:05 +0300

    Added Service search field to Backend Systems search form
    Resolves ASKI-279

---
+++ custom/modules/nlfbs_BackendSystems/metadata/searchdefs.php
@@ -0,0 +1,69 @@
+<?php
+$module_name = 'nlfbs_BackendSystems';
+$searchdefs [$module_name] = 
+array (
+  'layout' => 
+  array (
+    'basic_search' => 
+    array (
+      0 => 'name',
+      1 => 
+      array (
+        'name' => 'current_user_only',
+        'label' => 'LBL_CURRENT_USER_FILTER',
+        'type' => 'bool',
+      ),
+    ),
+    'advanced_search' => 
+    array (
+      'name' => 
+      array (
+        'name' => 'name',
+        'default' => true,
+        'width' => '10%',
+      ),
+      'related_service_enum' => 
+      array (
+        'type' => 'enum',
+        'studio' => 
+        array (
+          'detailview' => false,
+          'editview' => false,
+          'listview' => false,
+          'searchview' => true,
+        ),
+        'label' => 'LBL_RELATED_SERVICE_ENUM',
+        'width' => '10%',
+        'default' => true,
+        'name' => 'related_service_enum',
+      ),
+      'assigned_user_id' => 
+      array (
+        'name' => 'assigned_user_id',
+        'label' => 'LBL_ASSIGNED_TO',
+        'type' => 'enum',
+        'function' => 
+        array (
+          'name' => 'get_user_array',
+          'params' => 
+          array (
+            0 => false,
+          ),
+        ),
+        'default' => true,
+        'width' => '10%',
+      ),
+    ),
+  ),
+  'templateMeta' => 
+  array (
+    'maxColumns' => '3',
+    'maxColumnsBasic' => '4',
+    'widths' => 
+    array (
+      'label' => '10',
+      'field' => '30',
+    ),
+  ),
+);
+?>
