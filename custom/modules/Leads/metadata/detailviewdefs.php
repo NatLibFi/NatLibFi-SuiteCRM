3aad6835db73f74b418ec85cb9be49eb212b48a2
Leszek Manicki <lebiega@gmail.com>
2018-10-14 16:11:32 +0300

    Simplified Detail and Edit View of Leads
    Done by Susanna Eklund
    Part of ASKI-274

--- custom/modules/Leads/metadata/detailviewdefs.php
+++ custom/modules/Leads/metadata/detailviewdefs.php
@@ -85,22 +85,12 @@ array (
           'newTab' => false,
           'panelDefault' => 'expanded',
         ),
-        'LBL_EDITVIEW_PANEL2' => 
-        array (
-          'newTab' => false,
-          'panelDefault' => 'expanded',
-        ),
         'LBL_EDITVIEW_PANEL1' => 
         array (
           'newTab' => false,
           'panelDefault' => 'expanded',
         ),
         'LBL_PANEL_ADVANCED' => 
-        array (
-          'newTab' => false,
-          'panelDefault' => 'expanded',
-        ),
-        'LBL_PANEL_ASSIGNMENT' => 
         array (
           'newTab' => false,
           'panelDefault' => 'expanded',
@@ -127,31 +117,15 @@ array (
         array (
           0 => 
           array (
-            'name' => 'account_name',
-          ),
-        ),
-        2 => 
-        array (
-          0 => '',
-          1 => 
-          array (
             'name' => 'assigned_user_name',
             'label' => 'LBL_ASSIGNED_TO',
           ),
         ),
-        3 => 
-        array (
-          0 => 
-          array (
-            'name' => 'alliances_c',
-            'label' => 'LBL_ALLIANCES',
-          ),
-        ),
-        4 => 
+        2 => 
         array (
           0 => 'description',
         ),
-        5 => 
+        3 => 
         array (
           0 => 
           array (
@@ -160,7 +134,7 @@ array (
           ),
           1 => '',
         ),
-        6 => 
+        4 => 
         array (
           0 => 
           array (
@@ -169,119 +143,13 @@ array (
             'label' => 'LBL_COMMERCIAL_DESCRIPTION',
           ),
         ),
-        7 =>
-        array (
-          0 =>
-          array (
-            'name' => 'service_mail_link1',
-            'label' => 'LBL_SERVICE_MAIL_LINK_TITLE',
-          ),
-        ),
-      ),
-      'lbl_editview_panel2' => 
-      array (
-        0 => 
-        array (
-          0 => 'website',
-        ),
-        1 => 
-        array (
-          0 => 
-          array (
-            'name' => 'account_parent_c',
-            'studio' => 'visible',
-            'label' => 'LBL_ACCOUNT_PARENT',
-          ),
-        ),
-        2 => 
-        array (
-          0 => 
-          array (
-            'name' => 'hallinnonala_c',
-            'studio' => 'visible',
-            'label' => 'LBL_HALLINNONALA',
-          ),
-          1 => 
-          array (
-            'name' => 'sidosryhma_c',
-            'studio' => 'visible',
-            'label' => 'LBL_SIDOSRYHMA',
-          ),
-        ),
-        3 => 
-        array (
-          0 => 
-          array (
-            'name' => 'sektori_c',
-            'studio' => 'visible',
-            'label' => 'LBL_SEKTORI',
-          ),
-        ),
-        4 => 
-        array (
-          0 => 
-          array (
-            'name' => 'sic_code_c',
-            'label' => 'LBL_SIC_CODE',
-          ),
-          1 => 
-          array (
-            'name' => 'isil_c',
-            'label' => 'LBL_ISIL',
-          ),
-        ),
         5 => 
         array (
           0 => 
           array (
-            'name' => 'phone_office_c',
-            'label' => 'LBL_PHONE_OFFICE',
+            'name' => 'service_mail_link1',
+            'label' => 'LBL_SERVICE_MAIL_LINK_TITLE',
           ),
-          1 => 
-          array (
-            'name' => 'phone_alternate_c',
-            'label' => 'LBL_PHONE_ALTERNATE',
-          ),
-        ),
-        6 => 
-        array (
-          0 => 'account_email',
-        ),
-        7 => 
-        array (
-          0 => 
-          array (
-            'name' => 'account_backend_systems_c',
-            'studio' => 'visible',
-            'label' => 'LBL_ACCOUNT_BACKEND_SYSTEMS',
-          ),
-        ),
-        8 => 
-        array (
-          0 => 
-          array (
-            'name' => 'account_description',
-            'comment' => 'Description of lead account',
-            'label' => 'LBL_ACCOUNT_DESCRIPTION',
-          ),
-        ),
-        9 => 
-        array (
-          0 => 
-          array (
-            'name' => 'primary_address_street',
-            'label' => 'LBL_PRIMARY_ADDRESS',
-            'type' => 'address',
-            'displayParams' => 
-            array (
-              'key' => 'primary',
-            ),
-          ),
-        ),
-        10 => 
-        array (
-          0 => '',
-          1 => '',
         ),
       ),
       'lbl_editview_panel1' => 
@@ -294,49 +162,6 @@ array (
           ),
         ),
         1 => 
-        array (
-          0 => 
-          array (
-            'name' => 'first_name',
-            'comment' => 'First name of the contact',
-            'label' => 'LBL_FIRST_NAME',
-          ),
-          1 => 
-          array (
-            'name' => 'last_name',
-            'comment' => 'Last name of the contact',
-            'label' => 'LBL_LAST_NAME',
-          ),
-        ),
-        2 => 
-        array (
-          0 => 'phone_work',
-          1 => 
-          array (
-            'name' => 'phone_other',
-            'comment' => 'Other phone number for the contact',
-            'label' => 'LBL_OTHER_PHONE',
-          ),
-        ),
-        3 => 
-        array (
-          0 => 'email1',
-        ),
-        4 => 
-        array (
-          0 => 'title',
-          1 => 
-          array (
-            'name' => 'contact_account_role_c',
-            'studio' => 'visible',
-            'label' => 'LBL_CONTACT_ACCOUNT_ROLE',
-          ),
-        ),
-        5 => 
-        array (
-          0 => 'department',
-        ),
-        6 => 
         array (
           0 => 
           array (
@@ -379,14 +204,6 @@ array (
             'label' => 'LBL_DATE_MODIFIED',
             'customCode' => '{$fields.date_modified.value} {$APP.LBL_BY} {$fields.modified_by_name.value}',
           ),
-        ),
-      ),
-      'LBL_PANEL_ASSIGNMENT' => 
-      array (
-        0 => 
-        array (
-          0 => '',
-          1 => '',
         ),
       ),
     ),
