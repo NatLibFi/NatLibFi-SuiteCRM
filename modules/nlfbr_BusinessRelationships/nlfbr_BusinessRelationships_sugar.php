330dd10a70f7db8990f78b99970269b77905e40a
Leszek Manicki <lebiega@gmail.com>
2019-01-17 22:30:18 +0200

    Only join on finna view tables in BR list view when needed for filtering
    Part of ASKI-335

--- modules/nlfbr_BusinessRelationships/nlfbr_BusinessRelationships_sugar.php
+++ modules/nlfbr_BusinessRelationships/nlfbr_BusinessRelationships_sugar.php
@@ -84,7 +84,7 @@ class nlfbr_BusinessRelationships_sugar extends Basic {
     public function create_new_list_query($order_by, $where, $filter = array(), $params = array(), $show_deleted = 0, $join_type = '', $return_array = false, $parentbean = null, $singleSelect = false, $ifListForExport = false) {
          $ret_array = parent::create_new_list_query($order_by, $where, $filter, $params, $show_deleted, $join_type, true, $parentbean, $singleSelect, $ifListForExport);
 
-         if (strpos($ret_array['from'],'nlfbr_businessrelationships_finna_views') === false) {
+         if (strpos($ret_array['where'],'nlfbr_businessrelationships_finna_views') !== false && strpos($ret_array['from'],'nlfbr_businessrelationships_finna_views') === false) {
              $ret_array['from'] .= ' LEFT JOIN nlfbr_businessrelationships_finna_views ON (nlfbr_businessrelationships.id=nlfbr_businessrelationships_finna_views.businessrelationship_id AND nlfbr_businessrelationships_finna_views.deleted=0) ';
          }
 
