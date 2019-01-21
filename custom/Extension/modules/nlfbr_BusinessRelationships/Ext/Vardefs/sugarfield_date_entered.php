ef8e3ae6e51d76cd516d981ff5eb587a929bd8ff
Leszek Manicki <lebiega@gmail.com>
2019-01-21 22:43:36 +0200

    Do not allow massupdating of BR's date_entered field
    Related to ASKI-337

--- custom/Extension/modules/nlfbr_BusinessRelationships/Ext/Vardefs/sugarfield_date_entered.php
+++ custom/Extension/modules/nlfbr_BusinessRelationships/Ext/Vardefs/sugarfield_date_entered.php
@@ -1,6 +1,6 @@
 <?php
- // created: 2017-04-26 17:26:53
-$dictionary['nlfbr_BusinessRelationships']['fields']['date_entered']['massupdate']='1';
+ // created: 2019-01-21 22:36:43
+$dictionary['nlfbr_BusinessRelationships']['fields']['date_entered']['massupdate']=0;
 $dictionary['nlfbr_BusinessRelationships']['fields']['date_entered']['comments']='Date record created';
 $dictionary['nlfbr_BusinessRelationships']['fields']['date_entered']['merge_filter']='disabled';
 
