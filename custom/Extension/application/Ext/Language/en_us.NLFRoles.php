e1ab4508fccb7a196e57b8ba2cb5868a184fb47b
Leszek Manicki <lebiega@gmail.com>
2019-01-31 20:48:45 +0100

    Added undefined English text to Finnish-only labels

--- custom/Extension/application/Ext/Language/en_us.NLFRoles.php
+++ custom/Extension/application/Ext/Language/en_us.NLFRoles.php
@@ -44,3 +44,4 @@ $app_strings['LBL_ROLE_LIST_SUFFIX_ACCOUNT'] = 'Account';
 $app_strings['LBL_ROLE_LIST_SUFFIX_ALLIANCE'] = 'Alliance';
 $app_strings['LBL_ROLE_LIST_SUFFIX_BUSINESS_RELATIONSHIP'] = 'Business Relationship';
 $app_strings['LBL_ROLE_LIST_SUFFIX_WORKING_GROUP'] = 'Working Group';
+$app_strings['LBL_ROLE_LIST_SUFFIX_LEAD'] = 'in Lead';
