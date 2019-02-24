a5c4f2733bf0faea804e2f69c0066e3ad34f8cf7
Leszek Manicki <lebiega@gmail.com>
2019-02-24 13:07:04 +0200

    Removed unused code in Account's Contract subpanel definition

--- custom/Extension/modules/Accounts/Ext/Layoutdefs/accounts_contracts.php
+++ custom/Extension/modules/Accounts/Ext/Layoutdefs/accounts_contracts.php
@@ -1,12 +1,10 @@
 <?php
- // created: 2017-03-07 23:06:24
 $layout_defs["Accounts"]["subpanel_setup"]['accounts_contracts'] = array (
   'order' => 4,
   'module' => 'AOS_Contracts',
   'subpanel_name' => 'default',
   'sort_order' => 'asc',
   'sort_by' => 'id',
-  //'title_key' => 'LBL_ACCOUNTS_AOS_CONTRACTS_1_FROM_AOS_CONTRACTS_TITLE',
   'title_key' => 'AOS_Contracts',
   'get_subpanel_data' => 'function:getActiveContractsRelatedToAccountForSubpanelQueryParts',
   'function_parameters' => array(
@@ -15,15 +13,4 @@ $layout_defs["Accounts"]["subpanel_setup"]['accounts_contracts'] = array (
   ),
   'generate_select' => true,
   'top_buttons' => '',
-  /*array (
-    0 => 
-    array (
-      'widget_class' => 'SubPanelTopButtonQuickCreate',
-    ),
-    1 => 
-    array (
-      'widget_class' => 'SubPanelTopSelectButton',
-      'mode' => 'MultiSelect',
-    ),
-  ),*/
 );
