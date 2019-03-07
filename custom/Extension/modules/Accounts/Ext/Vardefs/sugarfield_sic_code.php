f4704eea0cfb27e73dd178e04502f4f33a1b4d45
Leszek Manicki <lebiega@gmail.com>
2019-03-07 22:22:42 +0200

    Extend SIC Code/Y-Tunnus field length (Accounts) to 20 characters
    Change by Susanna Eklund
    Fixes ASKI-355

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_sic_code.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_sic_code.php
@@ -1,8 +1,9 @@
 <?php
- // created: 2018-03-27 20:35:40
+ // created: 2019-03-07 10:49:21
 $dictionary['Account']['fields']['sic_code']['audited']=true;
 $dictionary['Account']['fields']['sic_code']['inline_edit']=true;
 $dictionary['Account']['fields']['sic_code']['comments']='SIC code of the account';
 $dictionary['Account']['fields']['sic_code']['merge_filter']='disabled';
+$dictionary['Account']['fields']['sic_code']['len']='20';
 
  ?>
