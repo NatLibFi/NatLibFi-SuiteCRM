4a7fdf700c4c531a03c5e9ac7b96627a6137c00c
Leszek Manicki <lebiega@gmail.com>
2019-03-10 20:01:42 +0200

    Adjusted Compose Group Email Widget to the new Compose email dialog

--- custom/include/generic/SugarWidgets/SugarWidgetSubPanelTopComposeGroupEmailButton.php
+++ custom/include/generic/SugarWidgets/SugarWidgetSubPanelTopComposeGroupEmailButton.php
@@ -6,7 +6,7 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 // related Contact(s), that are connected to the module through Roles.
 
 require_once 'include/generic/SugarWidgets/SugarWidgetSubPanelTopComposeEmailButton.php';
-require_once 'custom/include/GroupEmailHelper.php';
+require_once 'custom/include/EmailRecipientProvider.php';
 
 class SugarWidgetSubPanelTopComposeGroupEmailButton extends SugarWidgetSubPanelTopComposeEmailButton
 {
@@ -18,7 +18,7 @@ class SugarWidgetSubPanelTopComposeGroupEmailButton extends SugarWidgetSubPanelT
 			return $temp;
 		}
 		
-		global $app_strings,$current_user,$sugar_config,$beanList,$beanFiles;
+		global $app_strings,$current_user,$sugar_config,$beanList,$beanFiles, $locale;
 		$title = $app_strings['LBL_COMPOSE_EMAIL_BUTTON_TITLE'];
 		//$accesskey = $app_strings['LBL_COMPOSE_EMAIL_BUTTON_KEY'];
 		$value = $app_strings['LBL_COMPOSE_EMAIL_BUTTON_LABEL'];
@@ -33,17 +33,27 @@ class SugarWidgetSubPanelTopComposeGroupEmailButton extends SugarWidgetSubPanelT
 		} else {
 			$client = $defaultPref;
 		}
+        $bean = $defines['focus'];
+
 		if($client != 'sugar') {
-			$bean = $defines['focus'];
                         $to_addrs = 'foo@TODO.test'; // TODO: add code grabbing email(s) from contact(s)
 			$button = "<input class='button' type='button'  value='$value'  id='". $this->getWidgetId() . "'  name='".preg_replace('[ ]', '', $value)."'   title='$title' onclick=\"location.href='mailto:$to_addrs';return false;\" />";
 		} else {
-			//Generate the compose package for the quick create options.
-    			$composeData = array("parent_id" => $parent_id, "parent_type"=>$parent_type);
-			$emailHelper = new GroupEmailHelper();
-			$quickComposeOptions = $emailHelper->generateComposePackageForQuickCreate($composeData, http_build_query($composeData));
+            $recipientProvider = new EmailRecipientProvider(DBManagerFactory::getInstance(), $locale);
+            $recipientData = $recipientProvider->getRecipientNamesAndAddresses($bean->module_name, [ $bean->id ]);
+            $name = '';
+            $email = '';
+            if (!empty($recipientData)) {
+                $name = $recipientData[0]['name'];
+                $email = $recipientData[0]['email_address'];
+            }
 
-			$button = "<input title='$title'  id='". $this->getWidgetId()."'  onclick='SUGAR.quickCompose.init($quickComposeOptions);' class='button' type='submit' name='".preg_replace('[ ]', '', $value)."_button' value='$value' />";
+            $button = '<div type="hidden" onclick="$(document).openComposeViewModal(this);" data-module="' .
+                $bean->module_name . '" data-record-id="' .
+                $bean->id . '" data-module-name="' .
+                htmlspecialchars($name) .'" data-email-address="' .
+                $email .'">' .
+                '<a id="' . $this->getWidgetId() . '">' . $value  . '</a>';
 		}
 		return $button;
 	}
