60b5e2ac73161e8fd6b679f6c8a55cf10ffdb8f7
Leszek Manicki <lebiega@gmail.com>
2019-03-15 17:51:37 +0200

    Use the same action menu for Accounts as other modules have

--- custom/modules/Accounts/CustomAccountsListViewSmarty.php
+++ custom/modules/Accounts/CustomAccountsListViewSmarty.php
@@ -6,57 +6,6 @@ require_once('modules/Accounts/AccountsListViewSmarty.php');
 
 class CustomAccountsListViewSmarty extends AccountsListViewSmarty {
 
-        // NOTE: this is quite fragile. This assumes first action is Delete
-        // and the second one is Compose Email, and switches them
-	protected function buildActionsLink($id = 'actions_link', $location = 'top') {
-		$ret = parent::buildActionsLink($id, $location);
-
-		$replaces = array(
-			0 => 1,
-		);
-
-		foreach($replaces as $i => $j) {
-			$tmp = $ret['buttons'][$j];
-			$ret['buttons'][$j] = $ret['buttons'][$i];
-			$ret['buttons'][$i] = $tmp;
-		}
-
-        //$ret['buttons'][] = $this->buildComposeEmailToContactsInRoleLink('account_johtaja', $this->data['pageData']['offsets']['total'], $location);
-       // $ret['buttons'][] = $this->buildComposeEmailToContactsInRoleLink('account_asiantuntija', $this->data['pageData']['offsets']['total'], $location);
-
-		return $ret;
-	}
-
-    /**
-     * Based on ListViewDisplay::buildComposedEmailLink
-     */
-	private function buildComposeEmailToContactsInRoleLink($role, $totalCount, $loc = 'top')
-	{
-		global $app_strings,$dictionary, $mod_strings;
-
-        if (!is_array($this->seed->field_defs)) {
-            return '';
-        }
-
-		$userPref = $GLOBALS['current_user']->getPreference('email_link_type');
-		$defaultPref = $GLOBALS['sugar_config']['email_default_client'];
-		if($userPref != '')
-			$client = $userPref;
-		else
-			$client = $defaultPref;
-
-		if($client !== 'sugar') {
-            return '';
-        }
-
-			$script = "<a href='javascript:void(0)' " .
-                    "id=\"composeemail_" . $role . "_listview_". $loc ."\" ".
-					'onclick="return sListView.send_form_for_emails_contact_role(true, \''."Emails".'\', \'index.php?module=Emails&action=Compose&ListView=true&contactRole=\',\'' . $role . '\',\''.$app_strings['LBL_LISTVIEW_NO_SELECTED'].'\', \''.$this->seed->module_dir.'\', \''.$totalCount.'\', \''.$app_strings['LBL_LISTVIEW_LESS_THAN_TEN_SELECT'].'\')">' .
-					$mod_strings['LBL_EMAIL_COMPOSE_CONTACT_ROLE_' . $role] . '</a>';
-
-        return $script;
-	}
-
     protected function buildMassUpdateLink($loc = 'top')
     {
         global $app_strings;
