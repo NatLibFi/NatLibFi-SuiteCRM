d874b5f4952b16b72b66d71076c6af8a5c190c14
Leszek Manicki <lebiega@gmail.com>
2019-03-24 19:58:29 +0200

    Include selection on other list pages in all list views

--- custom/modules/Project/CustomProjectListViewSmarty.php
+++ custom/modules/Project/CustomProjectListViewSmarty.php
@@ -13,6 +13,25 @@ class CustomProjectListViewSmarty extends ProjectListViewSmarty {
 
     }
 
+    // Custom implementation, includes number of item selected in other pages of the list view, instead of always showing zero initially
+    function buildSelectLink($id = 'select_link', $total=0, $pageTotal=0, $location="top") {
+        $linkData = parent::buildSelectLink($id, $total, $pageTotal, $location);
+
+        $uids = array();
+        if (isset($_REQUEST['uid'])) {
+            $uids = explode(',', $_REQUEST['uid']);
+        }
+        $titleButton = '<label class="hidden glyphicon bootstrap-checkbox glyphicon-unchecked"></label>' .
+            '<input title="' . $app_strings['LBL_SELECT_ALL_TITLE'] . '" ' .
+            'type="checkbox" class="bootstrap-checkbox-hidden checkbox massall" name="massall" id="massall_"' . $location . '" ' .
+            'value=" onclick="sListView.check_all(document.MassUpdate, \'mass[]\', this.checked);" />' .
+            $this->buildSelectedObjectsSpan(true, count($uids)) . 
+            '<a id="' . $id . '" href="javascript: void(0);"></a>';
+
+        $linkData['buttons'][0] = $titleButton;
+        return $linkData;
+    }
+
 }
 
 ?>
