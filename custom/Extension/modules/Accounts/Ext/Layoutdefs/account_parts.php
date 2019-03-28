ae6f4f31cb9a6e5b1d8ac8e5474f6426eced5f0c
Leszek Manicki <lebiega@gmail.com>
2019-03-28 22:17:51 +0200

    Switched subpanel buttons of Account parts subpanel of Account view
    Part of ASKI-362

---
+++ custom/Extension/modules/Accounts/Ext/Layoutdefs/account_parts.php
@@ -0,0 +1,13 @@
+<?php
+$layout_defs['Accounts']['subpanel_setup']['accounts']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
