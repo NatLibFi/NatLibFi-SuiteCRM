5922046b82cf48ea1a0aa23d774fc086c35644a6
Leszek Manicki <lebiega@gmail.com>
2019-03-29 10:51:19 +0200

    Disabled unused NLFRoles module

---
+++ custom/Extension/application/Ext/Language/en_us.contact_roles.php
@@ -0,0 +1,7 @@
+<?php
+
+$app_strings['LBL_ROLE_LIST_SUFFIX_ACCOUNT'] = 'Account';
+$app_strings['LBL_ROLE_LIST_SUFFIX_ALLIANCE'] = 'Alliance';
+$app_strings['LBL_ROLE_LIST_SUFFIX_BUSINESS_RELATIONSHIP'] = 'Business Relationship';
+$app_strings['LBL_ROLE_LIST_SUFFIX_WORKING_GROUP'] = 'Working Group';
+$app_strings['LBL_ROLE_LIST_SUFFIX_LEAD'] = 'in Lead';
