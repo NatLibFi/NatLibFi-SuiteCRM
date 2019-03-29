b2f02363154f08bbfbcc03a257009a899b4bb0c3
Leszek Manicki <lebiega@gmail.com>
2019-03-29 10:51:19 +0200

    Disabled unused NLFRoles module

--- custom/Extension/application/Ext/Include/NLFRoles.php
+++
@@ -1,7 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-$beanList['nlfro_Roles'] = 'nlfro_Roles';
-$beanFiles['nlfro_Roles'] = 'modules/nlfro_Roles/nlfro_Roles.php';
-$moduleList[] = 'nlfro_Roles';
-
-?>
--- custom/Extension/application/Ext/Language/fi_FI.contact_roles.php
+++ custom/Extension/application/Ext/Language/fi_FI.contact_roles.php
@@ -1,3 +1,9 @@
 <?php
 
 $app_strings['LBL_CONTACT_ROLE_ADD_BUTTON_TITLE'] = 'Lisää tai muokkaa';
+
+$app_strings['LBL_ROLE_LIST_SUFFIX_ACCOUNT'] = 'organisaatiossa';
+$app_strings['LBL_ROLE_LIST_SUFFIX_ALLIANCE'] = 'yhteenliittymässä';
+$app_strings['LBL_ROLE_LIST_SUFFIX_BUSINESS_RELATIONSHIP'] = 'asiakassuhteessa';
+$app_strings['LBL_ROLE_LIST_SUFFIX_WORKING_GROUP'] = 'työryhmässä';
+$app_strings['LBL_ROLE_LIST_SUFFIX_LEAD'] = 'potentiaalisessa asiakassuhteessa';
