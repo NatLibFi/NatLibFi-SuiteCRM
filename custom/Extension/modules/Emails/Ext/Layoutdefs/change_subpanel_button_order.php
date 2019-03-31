1502d416e568c85edd58186d4606415a2fae07fb
Leszek Manicki <lebiega@gmail.com>
2019-03-31 19:28:19 +0300

    Reordered subpanel buttons of the subpanels of Email
    Part of ASKI-362

---
+++ custom/Extension/modules/Emails/Ext/Layoutdefs/change_subpanel_button_order.php
@@ -0,0 +1,79 @@
+<?php
+
+$layout_defs['Emails']['subpanel_setup']['accounts']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['Emails']['subpanel_setup']['contacts']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['Emails']['subpanel_setup']['leads']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['Emails']['subpanel_setup']['cases']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['Emails']['subpanel_setup']['bugs']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['Emails']['subpanel_setup']['project']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
