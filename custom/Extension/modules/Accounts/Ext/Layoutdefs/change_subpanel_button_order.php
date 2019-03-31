42181309252d6f411ad5df6d90843717d75c1718
Leszek Manicki <lebiega@gmail.com>
2019-03-31 18:58:41 +0300

    Adjusted subpanel buttons in Account subpanels

---
+++ custom/Extension/modules/Accounts/Ext/Layoutdefs/change_subpanel_button_order.php
@@ -0,0 +1,79 @@
+<?php
+
+$layout_defs['Accounts']['subpanel_setup']['documents']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['Accounts']['subpanel_setup']['cases']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['Accounts']['subpanel_setup']['account_aos_quotes']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['Accounts']['subpanel_setup']['account_aos_invoices']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['Accounts']['subpanel_setup']['bugs']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['Accounts']['subpanel_setup']['project']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
