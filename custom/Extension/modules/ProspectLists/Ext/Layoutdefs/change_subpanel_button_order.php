80257795d2bd590f2ad34bb45cd446ff0419c39b
Leszek Manicki <lebiega@gmail.com>
2019-03-31 19:22:27 +0300

    Reordered buttons in the subpanels of a Prospect List
    Part of ASKI-362

---
+++ custom/Extension/modules/ProspectLists/Ext/Layoutdefs/change_subpanel_button_order.php
@@ -0,0 +1,54 @@
+<?php
+
+$layout_defs['ProspectLists']['subpanel_setup']['prospects']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['ProspectLists']['subpanel_setup']['contacts']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['ProspectLists']['subpanel_setup']['leads']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['ProspectLists']['subpanel_setup']['accounts']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
