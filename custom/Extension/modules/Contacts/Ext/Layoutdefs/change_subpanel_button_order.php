d47e9c8697e07bf3793b6895993c23be2f820d8c
Leszek Manicki <lebiega@gmail.com>
2019-03-31 19:10:36 +0300

    Changed order of subpanel buttons in subpanels of a Contact
    Part of ASKI-362

---
+++ custom/Extension/modules/Contacts/Ext/Layoutdefs/change_subpanel_button_order.php
@@ -0,0 +1,53 @@
+<?php
+
+$layout_defs['Contacts']['subpanel_setup']['documents']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['Contacts']['subpanel_setup']['contact_aos_contracts']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['Contacts']['subpanel_setup']['fp_events_contacts']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
+
+$layout_defs['Contacts']['subpanel_setup']['project']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
