a6d5510a6799b4137715a26ea0b518f325467224
Leszek Manicki <lebiega@gmail.com>
2019-03-31 19:15:12 +0300

    Reordered buttons in the Documents subpanel of a Lead
    Part of ASKI-362

---
+++ custom/Extension/modules/Leads/Ext/Layoutdefs/change_subpanel_button_order.php
@@ -0,0 +1,14 @@
+<?php
+
+$layout_defs['Leads']['subpanel_setup']['fp_events_leads_1']['top_buttons'] =
+array (
+  0 =>
+  array (
+    'widget_class' => 'SubPanelTopSelectButton',
+    'mode' => 'MultiSelect',
+  ),
+  1 => 
+  array (
+    'widget_class' => 'SubPanelTopButtonQuickCreate',
+  )
+);
